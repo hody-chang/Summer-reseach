db8ccdba10cd5b4f1592a954ae14145a01f77137
hody-chang <107836041+hody-chang@users.noreply.github.com>
2022-06-25 08:09:44 +0100

    Delete Untitled.m

--- Untitled.m
+++
@@ -1,3 +0,0 @@
-git = settings().matlab.sourcecontrol.git;
-git.PrivateKeyFile.PersonalValue = 'C:\Users\aresm\.ssh\id_ed25519';
-git.PublicKeyFile.PersonalValue = 'C:\Users\aresm\.ssh\id_ed25519.pub';
